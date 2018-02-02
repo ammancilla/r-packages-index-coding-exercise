c14dcbfc9842c1a0ee7e44bfdf653a104e922809
Alfonso Mancilla <almancill@gmail.com>
2018-02-02 21:55:00 +0100

    Minor change

--- app/workers/packages_index_worker.rb
+++ app/workers/packages_index_worker.rb
@@ -1,7 +1,7 @@
 class PackagesIndexWorker
   include Sidekiq::Worker
 
-  def perform(limit = nil)
+  def perform
     CranServer.new.package_list.each_with_index do |package, i|
       name = package['Package']
       version = package['Version']
@@ -9,8 +9,6 @@ class PackagesIndexWorker
       next if name.blank? || version.blank?
 
       PackageRegisterWorker.perform_async(name, version)
-
-      break if limit.present? && i == (limit - 1)
     end
   end
 end
