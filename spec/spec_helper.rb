9f7cb75236e76185e5ec3d4a96968866a2a182cf
Alfonso Mancilla <almancill@gmail.com>
2018-02-02 16:05:28 +0100

    Initialize Rspec. Add initial (custom default) configuration.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'factory_bot'
+
+RSpec.configure do |config|
+  config.filter_run_when_matching :focus
+  config.include FactoryBot::Syntax::Methods
+  config.order = :random
+  config.profile_examples = 5
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+end
