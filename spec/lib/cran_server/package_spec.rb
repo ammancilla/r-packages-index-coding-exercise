f30b49dbf0e97f1dfb308887a540720860784995
Alfonso Mancilla <almancill@gmail.com>
2018-02-02 21:00:02 +0100

    Improve code redability

--- spec/lib/cran_server/package_spec.rb
+++ spec/lib/cran_server/package_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 describe CranServer::Package do
-  let(:expected_description) do
-    {:name=>"A3", :title=>"Accurate, Adaptable, and Accessible Error Metrics for Predictive Models", :version=>"1.0.0", :authors=>["Scott Fortmann-Roe"], :maintainer=>{:name=>"Scott Fortmann-Roe", :email=>"scottfr@berkeley.edu"}, :description=>"Supplies tools for tabulating and analyzing the results of predictive models. The methods employed are applicable to virtually any predictive model and make comparisons between different methodologies straightforward.", :publication=>"2015-08-16 23:05:52"}
+  let!(:expected_description) do
+    {
+      name: "A3",
+      title: "Accurate, Adaptable, and Accessible Error Metrics for Predictive Models",
+      version: "1.0.0",
+      authors: [ { name: "Scott Fortmann-Roe"} ],
+      maintainers: [
+	{ name: "Scott Fortmann-Roe", email: "scottfr@berkeley.edu"}
+      ],
+      description: "Supplies tools for tabulating and analyzing the results of predictive models. The methods employed are applicable to virtually any predictive model and make comparisons between different methodologies straightforward.",
+      publication: "2015-08-16 23:05:52"
+    }
   end
 
   describe ".new", vcr: { cassette_name: 'package file' } do
     it 'returns a new CranServer::Package instance with properly parsed attributes' do
       package_file = CranServer.new.fetch_package_file('A3', '1.0.0')
 
-      expect( described_class.new(package_file).description ).to eq(expected_description)
+      expect( CranServer::Package.new(package_file).description ).to eq(expected_description)
     end
   end
 end
